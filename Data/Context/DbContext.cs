89d2653c3afe3b7526e8f427480c52297b47f186
Miguel de Souza Silva <miguel.silva@mstrategy.com.br>
2025-11-28 16:36:29 -0300

    merge

---
+++ Data/Context/DbContext.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WebApplication1.Data.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Processo> Processos { get; set; }
+        public DbSet<Andamento> Andamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuração de relacionamento Cliente -> Processos
+            modelBuilder.Entity<Cliente>()
+                .HasMany(c => c.Processos)
+                .WithOne(p => p.Cliente)
+                .HasForeignKey(p => p.ClienteId)
+                .OnDelete(DeleteBehavior.Restrict); // Evita deletar Cliente se tiver processos ativos (Segurança)
+
+            // Configuração de relacionamento Processo -> Andamentos
+            modelBuilder.Entity<Processo>()
+                .HasMany(p => p.Andamentos)
+                .WithOne(a => a.Processo)
+                .HasForeignKey(a => a.ProcessoId)
+                .OnDelete(DeleteBehavior.Cascade); // Deletou processo, somem os andamentos
+        }
+    }
+}
